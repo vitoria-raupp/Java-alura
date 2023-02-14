488ca743c5bd640be8ec1bd54c2cafe4610430c5
Vitória Raupp <82777452+vitoria-raupp@users.noreply.github.com>
2023-02-14 20:05:41 -0300

    Create testeIf.java

---
+++ testeIf.java
@@ -0,0 +1,28 @@
+package sintaxe.variaveis.e.fluxo;
+
+public class TestaCondicional {
+	public static void main(String[] args) {
+		System.out.println("testando condicionais (if) ");
+		
+		int idade = 16;
+		int quantidadePessoas = 1;
+		
+		/*if (idade >= 18) {
+			System.out.println("Voce tem mais de 18 anos");
+		} else {
+			if(quantidadePessoas >= 2) {
+				System.out.println("Voce nao tem 18 ano, mas esta acompanhado");
+			}else {
+				System.out.println("voce e menor de idade");
+			}
+		}
+	}*/
+	
+	
+	if (idade >= 18 || quantidadePessoas >= 2 ) {
+		System.out.println("Seja bem vindo");
+	} else {
+		System.out.println("infelizmente vc nao pode entrar");
+	}
+}
+}
