284047c12796bee70eece0c88aac526863b3d7c8
Vitória Raupp <82777452+vitoria-raupp@users.noreply.github.com>
2023-02-27 10:53:48 -0300

    Add files via upload

--- Conta.java
+++ Conta.java
@@ -1,10 +1,19 @@
 package byteBank;
 
 public class Conta {
-	double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	public Conta(int agencia, int numero){
+		Conta.total++;
+		System.out.println("Ototal de contas é: " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		
+	}
 	
 	public void deposita(double valor) {
 		this.saldo = this.saldo + valor;
@@ -28,4 +37,43 @@ public class Conta {
 		return false; 
 	}
 	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		if(numero <= 0) {
+			System.out.println("Numero invalido");
+			return;
+		}
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		if(agencia <=0) {
+			System.out.println("Agencia invalida");
+			return;
+		}
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return this.titular;
+	}
+	
+	public int getTotal() {
+		return Conta.total;
+	}
 }
